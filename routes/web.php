abbf2ecb95de483ae996bb466d1cc5129abd7c8c
Oluwatobi Ishola <ishoshot@gmail.com>
2024-10-02 21:20:37 +0100

    update home page route

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,13 @@
 
 declare(strict_types=1);
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (): JsonResponse {
+    return response()->json([
+        'app' => 'News Aggregator API',
+        'version' => '0.0.1 alpha',
+        'disclaimer' => 'This application is a property of Oluwatobi Ishola for Innoscripta AG.',
+    ]);
 });
