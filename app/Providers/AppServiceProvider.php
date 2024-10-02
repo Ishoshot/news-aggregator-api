a9c831b5fc91beab22d44f1316d8999ee96ff43a
Oluwatobi Ishola <ishoshot@gmail.com>
2024-10-02 20:29:44 +0100

    add dedicated rate limit for login endpoint

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 final class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +25,21 @@ final class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->configureRateLimits();
+    }
+
+    /**
+     * Configure rate limits.
+     */
+    private function configureRateLimits(): void
+    {
+        RateLimiter::for('login', function (Request $request): Limit {
+
+            $email = type($request->input('email'))->asString();
+
+            $throttleKey = Str::transliterate(Str::lower($email).'|'.$request->ip());
+
+            return Limit::perMinute(5)->by($throttleKey);
+        });
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,6 @@ Route::middleware('throttle:12,1')->prefix('auth')->group(function (): void {
 
     Route::post('/register', [RegisteredUserController::class, 'store'])->name('auth.register');
 
-    Route::post('/login', [AuthenticatedUserController::class, 'store'])->name('auth.login');
+    Route::middleware('throttle:login')->post('/login', [AuthenticatedUserController::class, 'store'])->name('auth.login');
 
 });
