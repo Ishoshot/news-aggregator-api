11dcc64b3e5df5b000265efaa77d0f8282856eb9
Oluwatobi Ishola <ishoshot@gmail.com>
2024-10-02 21:25:32 +0100

    update http architectural test

--- tests/Arch/HttpTest.php
+++ tests/Arch/HttpTest.php
@@ -13,10 +13,10 @@ arch('controllers')
 //     ->toUse('Illuminate\Http\Request')
 //     ->not->toBeUsed();
 
-// arch('requests')
-//     ->expect('App\Http\Requests')
-//     ->toExtend('Illuminate\Foundation\Http\FormRequest')
-//     ->toHaveMethod('rules')
-//     ->toBeUsedIn([
-//         'App\Http\Controllers',
-//     ]);
+arch('requests')
+    ->expect('App\Http\Requests')
+    ->toExtend('Illuminate\Foundation\Http\FormRequest')
+    ->toHaveMethod('rules')
+    ->toBeUsedIn([
+        'App\Http\Controllers',
+    ]);
